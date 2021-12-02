acb980a6a50266a095f85fbd4f7c31f655d6bdd9
Ba <duongvanba.com@gmail.com>
2021-12-02 21:36:50 +0700

    Update

--- src/pathResolver.ts
+++ src/pathResolver.ts
@@ -1,17 +1,33 @@
 import { LIVEQUERY_MAGIC_KEY } from "./const"
 
-export const pathResolver = (target: Function, method: string) => {
 
-    const collection_path: string = Reflect.getMetadata('path', target) || ''
-    const method_path: string = Reflect.getMetadata('path', target.prototype[method]) || ''
+export class PathHelper {
 
-    const refs = `${collection_path}/${method_path}`
-        .replaceAll(':', '')
-        .split(LIVEQUERY_MAGIC_KEY)?.[1]
-        .split('/')
-        .filter(s => s.length > 0)
+    static #toArray = (a: string | string[]) => typeof a == 'string' ? [a] : a
 
-    const ref = refs.join('/')
-    const schema_ref = refs.filter((_, i) => i % 2 == 0).join('/')
-    return { ref, schema_ref }
+    static livequeryPathExtractor(path: string) {
+        const refs = path
+            ?.replaceAll(':', '')
+            ?.split(LIVEQUERY_MAGIC_KEY)?.[1]
+            ?.split('/')
+            ?.filter(s => s.length > 0)
+        if (!refs) return null
+        const ref = refs.join('/')
+        const schema_ref = refs.filter((_, i) => i % 2 == 0).join('/')
+        return { ref, schema_ref }
+
+    }
+
+    static nestjsPathResolver(target: Function, method: string) {
+        const collection_paths = this.#toArray(Reflect.getMetadata('path', target) || '')
+        const method_paths = this.#toArray(Reflect.getMetadata('path', target.prototype[method]) || '')
+        return collection_paths.map(
+            collection_path => method_paths.map(
+                method_path => `${collection_path}/${method_path}`
+            )
+        ).flat(2)
+    }
+
 }
+
+
