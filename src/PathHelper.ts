d0ee7fdb34426dcd330244884780d7d8519a9087
Ba <duongvanba.com@gmail.com>
2021-12-29 17:39:31 +0700

    Updae

--- src/PathHelper.ts
+++ src/PathHelper.ts
@@ -18,14 +18,14 @@ export class PathHelper {
         const ref = refs.join('/')
         const collection_ref = is_collection ? ref : refs.slice(0, refs.length - 1).join('/')
         const schema_ref = refs.filter((_, i) => i % 2 == 0).join('/')
-        const doc_id = is_collection ? refs[refs.length - 1] : null
+        const doc_id = is_collection ? null : refs[refs.length - 1]
 
         return { ref, schema_ref, is_collection, doc_id, collection_ref }
 
     }
 
     static nestjsPathResolver(target: Function, method: string) {
-        const collection_paths = this.#toArray(Reflect.getMetadata('path', target) || '') 
+        const collection_paths = this.#toArray(Reflect.getMetadata('path', target) || '')
         const method_paths = this.#toArray(Reflect.getMetadata('path', target.prototype[method]) || '')
         return collection_paths.map(
             collection_path => method_paths.map(
