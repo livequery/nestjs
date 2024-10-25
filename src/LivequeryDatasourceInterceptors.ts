75189157c869b277e9b7748474af0717c485ee28
Ba <ba@flygo.vn>
2024-10-25 22:38:39 +0700

    update item mapper

--- src/LivequeryDatasourceInterceptors.ts
+++ src/LivequeryDatasourceInterceptors.ts
@@ -1,15 +1,18 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { map } from 'rxjs';
+import { map, mergeMap, Observable } from 'rxjs';
 import { ModuleRef } from '@nestjs/core'
 import { LivequeryDatasource } from './helpers/createDatasourceMapper.js';
+import { LivequeryBaseEntity } from '@livequery/types';
 
 
 
 export const LivequeryDatasourceList = new Map<{ new(...args): LivequeryDatasource }, LivequeryDatasource>()
 export const $__datasource_factory_token = Symbol()
 
-
-
+export class LivequeryItemMapper<T extends LivequeryBaseEntity> {
+    constructor(public readonly mapper: (item: T) => T) { }
+}
+ 
 @Injectable()
 export class LivequeryDatasourceInterceptors implements NestInterceptor {
 
@@ -19,11 +22,36 @@ export class LivequeryDatasourceInterceptors implements NestInterceptor {
 
         const token = Reflect.getMetadata($__datasource_factory_token, ctx.getHandler())
         const datasource = LivequeryDatasourceList.has(token) ? LivequeryDatasourceList.get(token) : this.moduleRef.get(token)
-        const req = ctx.switchToHttp().getRequest()
-        req.livequery_response = await datasource.query(req.livequery)
+
+
         return next.handle().pipe(
-            map(rs => rs ?? req.livequery_response)
+            mergeMap(async rs => {
+                const req = ctx.switchToHttp().getRequest()
+                const lrs = await datasource.query(req.livequery)
+                
+                if (rs instanceof LivequeryItemMapper) {
+                    if (lrs.item) {
+                        return {
+                            ...lrs,
+                            item: rs.mapper(lrs.item)
+                        }
+                    }
+
+                    if (lrs.items) {
+                        return {
+                            ...lrs,
+                            items: lrs.items.map(item => rs.mapper(item))
+                        }
+                    }
+                    return lrs
+                }
+                
+                if (typeof rs == 'function') {
+                    return await rs(lrs)
+                }
+                return rs || lrs
+            })
         )
-        
+
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 export { listPaths } from './helpers/listPaths.js'
 
 export { LivequeryRequest } from './LivequeryRequest.js'
-export { LivequeryResponse } from './LivequeryResponse.js'
 export { LivequeryInterceptor, RealtimeSubscription, UseLivequeryInterceptor } from './LivequeryInterceptor.js'
 export { LivequeryWebsocketSync } from './LivequeryWebsocketSync.js'
 export { createDatasourceMapper, LivequeryDatasourceOptions, LivequeryDatasource } from './helpers/createDatasourceMapper.js'
-export { LivequeryDatasourceInterceptors } from './LivequeryDatasourceInterceptors.js'
+export { LivequeryDatasourceInterceptors, LivequeryItemMapper } from './LivequeryDatasourceInterceptors.js'
 export { UseWebsocketPublicKey, UseWebsocketPrivateKey } from './UseWebsocketShareKeyPair.js'
 export { ApiGateway } from './ApiGateway.js'
 export { ApiGatewayLinker } from './ApiGatewayLinker.js'
